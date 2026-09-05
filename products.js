7def02c66c5cec4b3a03981845a49415d14afe9f
speksk5029-hash <s.p.e.ksk5029@gmail.com>
2026-09-05 17:39:58 +0900

    トップページ文言と販売商品を更新し2商品の中央配置に対応

--- products.js
+++ products.js
@@ -1,46 +1,46 @@
 const PRODUCTS = [
   {
     colorClass: "c1",
-    name: "SLIM WALLET《ラッフィナート》",
-    price: "¥36,800",
+    name: "一枚革のキーケース　ミネルバボックス",
+    price: "¥8,800",
     badge: "前田工芸",
     badgeClass: "maeda-badge",
-    url: "https://toraleather.handcrafted.jp/items/122159269",
+    url: "https://shop.maedakogei.com/items/154538162",
     image:
-      "https://baseec-img-mng.akamaized.net/images/item/origin/5ba3e9227cc4873a53b82e86f867c3de.jpg?imformat=generic&q=90&im=Resize,width=1280,type=normal",
+      "https://baseec-img-mng.akamaized.net/images/item/origin/cbdbb952dbe6ef95ea3ade752146c99c.jpg?imformat=generic&q=90&im=Resize,width=1280,type=normal",
     sold: false,
   },
   {
     colorClass: "c2",
-    name: "COMPACT WALLET《タスカービレ》",
-    price: "¥32,800",
+    name: "一枚革のキーケース　プエブロレザー",
+    price: "¥8,800",
     badge: "前田工芸",
     badgeClass: "maeda-badge",
-    url: "https://toraleather.handcrafted.jp/items/122833786",
+    url: "https://shop.maedakogei.com/items/154535209",
     image:
-      "https://baseec-img-mng.akamaized.net/images/item/origin/51cbfade398c1747bab2b76ddc1eabc9.jpg?imformat=generic&q=90&im=Resize,width=1280,type=normal",
+      "https://baseec-img-mng.akamaized.net/images/item/origin/ac7bf468585070d680a37082e70b2c6c.jpg?imformat=generic&q=90&im=Resize,width=1280,type=normal",
     sold: false,
   },
-  {
-    colorClass: "c3",
-    name: "4 KEY CASE《クラシコ》",
-    price: "¥24,800",
-    badge: "前田工芸",
-    badgeClass: "maeda-badge",
-    url: "https://toraleather.handcrafted.jp/items/122033425",
-    image:
-      "https://baseec-img-mng.akamaized.net/images/item/origin/20b398ac27ead694dc86099a90a79531.jpg?imformat=generic&q=90&im=Resize,width=1280,type=normal",
-    sold: false,
-  },
-  {
-    colorClass: "c3",
-    name: "4 KEY CASE《クラシコ》",
-    price: "¥24,800",
-    badge: "前田工芸",
-    badgeClass: "maeda-badge",
-    url: "https://toraleather.handcrafted.jp/items/122033300",
-    image:
-      "https://baseec-img-mng.akamaized.net/images/item/origin/d3a7e46bb396d00b438d33fae78be7f4.jpg?imformat=generic&q=90&im=Resize,width=1280,type=normal",
-    sold: false,
-  },
+  // {
+  //   colorClass: "c3",
+  //   name: "4 KEY CASE《クラシコ》",
+  //   price: "¥24,800",
+  //   badge: "前田工芸",
+  //   badgeClass: "maeda-badge",
+  //   url: "https://toraleather.handcrafted.jp/items/122033425",
+  //   image:
+  //     "https://baseec-img-mng.akamaized.net/images/item/origin/20b398ac27ead694dc86099a90a79531.jpg?imformat=generic&q=90&im=Resize,width=1280,type=normal",
+  //   sold: false,
+  // },
+  // {
+  //   colorClass: "c4",
+  //   name: "4 KEY CASE《クラシコ》",
+  //   price: "¥24,800",
+  //   badge: "前田工芸",
+  //   badgeClass: "maeda-badge",
+  //   url: "https://toraleather.handcrafted.jp/items/122033300",
+  //   image:
+  //     "https://baseec-img-mng.akamaized.net/images/item/origin/d3a7e46bb396d00b438d33fae78be7f4.jpg?imformat=generic&q=90&im=Resize,width=1280,type=normal",
+  //   sold: false,
+  // },
 ];
